ed46083b19aa6f2a66455f82eece21288df9829f
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2016-05-07 22:26:47 +0600

    Adding Endpoints to the Server

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ npm init -y
 npm install --save express
 npm install -g nodemon
 ```
+
+### 2. Adding Endpoints to the Server
+```
+npm install --save lodash
+npm install --save body-parser
+```
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
-
 var app = express();
 
-app.get('/', function (req, res) {
-    res.json({hello: 'world'});
-});
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var cats = require('./cats.js')(app);
 
 var server = app.listen(3000, function() {
     console.log('Server running http://localhost:3000');
